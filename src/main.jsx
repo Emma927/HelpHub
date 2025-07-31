269d0298cf30366fbf604709baacea26a02f3aa4
Emma927 <emma.wojtyna@gmail.com>
2025-07-31 10:33:24 +0200

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '@/scss/main.scss'
-import App from '@/App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { AnnouncementsProvider } from '@/context/AnnouncementsContext';
+import { FiltersProvider } from '@/context/FiltersContext';
+import '@/scss/main.scss';
+import App from '@/App';
 
 createRoot(document.getElementById('app')).render(
-    <StrictMode>
-        <App/>
-    </StrictMode>,
-)
+  <StrictMode>
+    <FiltersProvider>
+      <AnnouncementsProvider>
+        <App />
+      </AnnouncementsProvider>
+    </FiltersProvider>
+  </StrictMode>,
+);
