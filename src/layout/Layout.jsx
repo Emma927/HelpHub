48dbcbd6cdb327754e72d5597033d9d4c67af7d6
Emma927 <emma.wojtyna@gmail.com>
2025-07-31 10:26:14 +0200

    Updated layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,15 +1,15 @@
-import Header from "@/components/Header";
-import Main from "@/components/Main";
-import Footer from "@/components/Footer";
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
+import Footer from '@/components/Footer';
 
 function Layout() {
-    return (
-        <>
-            <Header/>
-            <Main/>
-            <Footer/>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Footer />
+    </>
+  );
 }
 
 export default Layout;
