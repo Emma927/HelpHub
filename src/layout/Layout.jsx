8c64280e0a755b0c736bb385ee1dbf9c4125cf37
Emma927 <emma.wojtyna@gmail.com>
2025-10-17 19:25:08 +0200

    Updated project

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,17 +1,22 @@
 import Header from '@/components/Header';
-import Hero from '@/components/Hero';
+import { Outlet } from 'react-router-dom';
 import Footer from '@/components/Footer';
 
 /**
- * Komponent Hero wyświetla główną sekcję powitalną strony.
- * - Zawiera nagłówki, grafiki lub treści wprowadzające.
- * - Zawiera <Outlet />, który umożliwia renderowanie komponentów stron zagnieżdżonych (nested routes) w tym miejscu.
+ * Layout – główny komponent układu aplikacji.
+ * - Zawiera nagłówek (Header) i stopkę (Footer), które są stałe na wszystkich stronach.
+ * - Renderuje zawartość podstron przez <Outlet /> w głównej części strony (<main>).
+ * - Zapewnia marginesy i wyrównanie za pomocą klas CSS (flexbox, container, padding).
  */
 function Layout() {
   return (
     <>
       <Header />
-      <Hero />
+      <main className="d-flex flex-grow-1 justify-content-center pt-2">
+        <div className="container d-flex justify-content-center mt-5 pt-5">
+          <Outlet /> {/* Renderuje podstrony zależnie od aktualnej ścieżki */}
+        </div>
+      </main>
       <Footer />
     </>
   );
